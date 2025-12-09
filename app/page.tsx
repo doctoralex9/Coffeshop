7e5887346d3576a254fe7337a57958be5eddff30
Melas <doctoralex9@gmail.com>
2025-12-09 14:51:46 +0200

    remove unnecessary props from MenuPreview page

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function HomePage() {
   return(
     <>
       <Hero />
-      <MenuPreview coldCoffees={[]} hotCoffees={[]} />
+      <MenuPreview />
       <Footer />
     </>
   );
